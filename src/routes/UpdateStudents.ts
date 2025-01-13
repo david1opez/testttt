c9b649b70d73e12c09b307266d54fedf1568f5b0
David Lopez <58573376+sscampusmty@users.noreply.github.com>
2025-01-13 17:15:22 -0600

    Refactor UpdateStudents route to handle student data as objects and implement range updates in Google Sheets

--- src/routes/UpdateStudents.ts
+++ src/routes/UpdateStudents.ts
@@ -1,15 +1,16 @@
 import { doc, updateDoc } from "firebase/firestore";
 
 // HELPER FUNCTIONS
+import { updateSheet } from "../utils/google_sheets/GoogleSheets";
 import { db } from "../utils/firebase/config";
-import { Error, CheckForTimeout, SendResponse } from "../utils/express/Utils";
+import { Error, SendResponse } from "../utils/express/Utils";
 
 // TYPES
 import { Request, Response } from 'express';
 
 export default async function UpdateStudents(req: Request, res: Response) {    
     const { projectID } = req.query as { projectID: string };
-    const { students } = req.body as { students: (string|number|boolean)[][] };
+    const { students } = req.body as { students: {data: (string|number|boolean)[], row: number }[] };
 
     if(!students) {
         Error(res, 400, "Missing 'students' Body Parameter");
@@ -17,11 +18,52 @@ export default async function UpdateStudents(req: Request, res: Response) {
     }
 
     try {
-        let studentsObj: {[key: string]: (string|number|boolean)[]} = {};
+        let studentsObj: {[key: string]: {data: (string|number|boolean)[], row: number}} = {};
 
-        students.forEach((student) => {
-            studentsObj[student[0] as string] = student
+        let ranges: {students: (string|number|boolean)[][], range: [number, number]}[] = [];
+
+        let duplicatedStudents: string[] = [];
+
+        students.map((student) => {
+            if(studentsObj[student.data[0] as string]) {
+                duplicatedStudents.push(student.data[0] as string);
+                return;
+            };
+
+            studentsObj[student.data[0] as string] = {
+                data: student.data,
+                row: student.row
+            }
+
+            if(ranges.length === 0) {
+                ranges.push({
+                    students: [student.data],
+                    range: [student.row, student.row]
+                });
+            } else {
+                const lastIdx = ranges.length - 1;
+
+                if(student.row-1 === ranges[lastIdx].range[1]) {
+                    ranges[lastIdx].range[1] = student.row;
+                    ranges[lastIdx].students.push(student.data);
+                } else {
+                    ranges.push({
+                        students: [student.data],
+                        range: [student.row, student.row]
+                    });
+                }
+            }
         });
+
+        await Promise.all(ranges.map(async (range) => {
+            console.log(`Updating range A${range.range[0]}:G${range.range[1]}`);
+
+            await updateSheet(
+                process.env.SHEET_ID || "", "alumnos",
+                `A${range.range[0]}:G${range.range[1]}`,
+                range.students
+            );
+        }));
 
         const studentsRef = doc(db, "alumnos", projectID);
 
@@ -33,8 +75,15 @@ export default async function UpdateStudents(req: Request, res: Response) {
             }
         });
 
-        const updatedStudents = students.map((student) => student[0]);
+        const updatedStudents = students.map((student) => student.data[0]);
 
+        if(duplicatedStudents.length > 0) {
+            Error(res, 409, {
+                message: "Failed to update some students with duplicated IDs",
+                duplicatedStudents
+            });
+            return;
+        }
         SendResponse(res, 200, { updatedStudents });
     }
     catch (err) {
